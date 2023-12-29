62ce9e3a641167883fa340f367a934b2d4e804e5
gianghoang22 <lehoanggiang220602@gmail.com>
2023-12-29 17:15:27 +0700

    Remove lint-stage husky

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,19 +1,17 @@
-const Course = require('../models/Course')
+const Course = require('../models/Course');
 const { multipleMongooseToObject } = require('../../util/mongoose');
 class SiteController {
     //[GET] /
 
     async index(req, res, next) {
-        
         Course.find({})
-            .then(courses => {
-                res.render('home', { 
-                    courses: multipleMongooseToObject(courses)
-                 });
+            .then((courses) => {
+                res.render('home', {
+                    courses: multipleMongooseToObject(courses),
+                });
             })
             .catch(next);
 
-        
         // try {
         //     const data = await Course.find({});
         //     res.json(data);
--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
 
 async function connect() {
-
     try {
-await mongoose.connect('mongodb://127.0.0.1:27017/Project_courses_nodejs_dev', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-console.log('Connect successfully');
-    }catch (error) {
+        await mongoose.connect(
+            'mongodb://127.0.0.1:27017/Project_courses_nodejs_dev',
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            },
+        );
+        console.log('Connect successfully');
+    } catch (error) {
         console.log('Connect fail');
     }
-
 }
 
 module.exports = { connect };
