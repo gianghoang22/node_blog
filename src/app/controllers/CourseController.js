4876b17fc88b5915338fe2556525c5af48a45249
gianghoang22 <lehoanggiang220602@gmail.com>
2023-12-30 20:56:31 +0700

    (CRUD) Delete course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -18,6 +18,17 @@ class CourseController {
     res.render("courses/create");
   }
 
+    //[POST] /courses/store
+    store(req, res, next) {
+      // res.json(req.body);
+      const formData = req.body;
+      req.body.image = `https://files.fullstack.edu.vn/f8-prod/${req.body.videoId}/6.png`;
+      const course = new Course(formData);
+      course.save()
+        .then(() => res.redirect(`/`))
+        .catch((error) => {});
+    }
+
   //[GET] /courses/:id/edit
   edit(req, res, next) {
     Course.findById(req.params.id)
@@ -27,24 +38,20 @@ class CourseController {
     .catch(next);
   }
 
-  //[PUT] /courses/:id    - chinh sua
+  //[PUT] /courses/:id  - chinh sua
   update(req, res, next) {
     Course.updateOne({ _id: req.params.id }, req.body)
     .then(() => res.redirect('/me/stored/courses'))
     .catch(next);
   }
 
-  //[POST] /courses/store
-  store(req, res, next) {
-    // res.json(req.body);
-    const formData = req.body;
-    req.body.image = `https://files.fullstack.edu.vn/f8-prod/${req.body.videoId}/6.png`;
-    const course = new Course(formData);
-    course
-      .save()
-      .then(() => res.redirect(`/`))
-      .catch((error) => {});
+  //[DELETE] /course/:id - xoa
+  delete(req, res, next) {
+    Course.deleteOne({ _id: req.params.id }, req.body)
+    .then(() => res.redirect('back'))
+    .catch(next);
   }
+
 }
 
 module.exports = new CourseController();
