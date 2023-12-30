1ebc38348d615f932d06b0242ef0ab8ca8463b95
gianghoang22 <lehoanggiang220602@gmail.com>
2023-12-30 13:41:44 +0700

    (CRUD) Update course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,34 +1,50 @@
-const Course = require('../models/Course');
-const { mongooseToObject } = require('../../util/mongoose');
+const Course = require("../models/Course");
+const { mongooseToObject } = require("../../util/mongoose");
 
 class CourseController {
-    //[GET] /courses/:slug
-    show(req, res, next) {
-        Course.findOne({ slug: req.params.slug })
-            .then((course) => {
-                res.render('courses/show', {
-                    course: mongooseToObject(course),
-                });
-            })
-            .catch(next);
-    }
+  //[GET] /courses/:slug
+  show(req, res, next) {
+    Course.findOne({ slug: req.params.slug })
+      .then((course) => {
+        res.render("courses/show", {
+          course: mongooseToObject(course),
+        });
+      })
+      .catch(next);
+  }
 
-    //[GET] /courses/:create
-    create(req, res, next) {
-        res.render('courses/create');
-    }
+  //[GET] /courses/:create
+  create(req, res, next) {
+    res.render("courses/create");
+  }
 
-    //[POST] /courses/store
-    store(req, res, next) {
-        // res.json(req.body);
-        const formData = req.body;
-        req.body.image = `https://files.fullstack.edu.vn/f8-prod/${req.body.videoId}/6.png`;
-        const course = new Course(formData);
-        course
-            .save()
-            .then(() => res.redirect(`/`))
-            .catch((error) => {});
-    }
+  //[GET] /courses/:id/edit
+  edit(req, res, next) {
+    Course.findById(req.params.id)
+    .then(course => res.render('courses/edit', {
+      course: mongooseToObject(course)
+    }))
+    .catch(next);
+  }
+
+  //[PUT] /courses/:id    - chinh sua
+  update(req, res, next) {
+    Course.updateOne({ _id: req.params.id }, req.body)
+    .then(() => res.redirect('/me/stored/courses'))
+    .catch(next);
+  }
+
+  //[POST] /courses/store
+  store(req, res, next) {
+    // res.json(req.body);
+    const formData = req.body;
+    req.body.image = `https://files.fullstack.edu.vn/f8-prod/${req.body.videoId}/6.png`;
+    const course = new Course(formData);
+    course
+      .save()
+      .then(() => res.redirect(`/`))
+      .catch((error) => {});
+  }
 }
 
 module.exports = new CourseController();
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+const methodOverride = require('method-override');
 const app = express();
 
 app.use(express.json());
@@ -15,16 +16,19 @@ const db = require('./config/db');
 //connect to DB
 db.connect();
 
-app.use(express.static(path.join(__dirname, 'public')));
+//override
+app.use(methodOverride('_method'))
 
-//HTTP logger
-// app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Template Engine
 app.engine(
     'hbs',
     handlebars.engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        }
     }),
 );
 app.set('view engine', 'hbs');
